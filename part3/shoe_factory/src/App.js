da48767b8789a7a949bd59cdfbb7eac41635ba02
snehachitre-apps <sneha.chitre.usa@gmail.com>
2023-08-04 13:46:15 -0500

    shoe counter app

---
+++ part3/shoe_factory/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ShoeButton from './components/ShoeButton';
+
+function App() {
+  return (
+    <div className="App">
+      <ShoeButton/>
+    </div>
+  );
+}
+
+export default App;
